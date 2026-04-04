0b6319fe16273901acd883087e38111ae385421e
abhi-bish-codes <268784052+abhi-bish-codes@users.noreply.github.com>
2026-04-04 08:48:33 +0530

    2.x header project

--- multi_file_calculator/multi_file_calculator.cpp
+++ multi_file_calculator/multi_file_calculator.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <cstdio>
 
+//Declaring functions used
 int getInteger();
 int add(int x, int y);
 int subtract(int x, int y);
@@ -8,12 +10,25 @@ int divide(int x, int y);
 
 int main()
 {
+    //Input 2 integers from user and store them in x and y
     int x{getInteger()};
     int y{getInteger()};
+
+    std::cout << "=========================================================\n";
+
+    //Calculations
     std::cout << x << " + " << y << " is " << add(x,y) << "\n";
     std::cout << x << " - " << y << " is " << subtract(x,y) << "\n";
     std::cout << x << " * " << y << " is " << multiply(x,y) << "\n";
     std::cout << x << " / " << y << " is " << divide(x,y) << "\n";
+
+    std::cout << "=========================================================\n";
+
+    //Stops terminal for closing instantly
+    std::cout << "Enter any key to exit...\n";
+    std::getchar();
+    char character{};
+    std::cin >> character;
 
     return 0;
 }
